ded71f090a17a96d4dca63fc1e60e528b82281db
luca-bruno <lucabru@gmail.com>
2022-08-15 13:08:57 +0200

    Shadow on instruction prompt

--- trivya-frontend/src/InstructionPrompt.jsx
+++ trivya-frontend/src/InstructionPrompt.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const InstructionPrompt = ({ text }) => {
   return (
-    <div className='absolute m-auto left-0 right-0 pt-0.5 bg-secondary w-80 h-9 rounded-br-xl rounded-bl-xl'>
+    <div className='absolute m-auto left-0 right-0 pt-0.5 bg-secondary w-80 h-9 rounded-br-xl rounded-bl-xl shadow-lg'>
         <p className='text-primary text-center text-xl px-3 rounded-br-xl rounded-bl-xl'>
             {text}
         </p>
