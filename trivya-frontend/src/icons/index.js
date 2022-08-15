763dc13a9a69169dcbeddf79530817e495e5d434
luca-bruno <lucabru@gmail.com>
2022-08-15 10:02:00 +0200

    Misc asset file

---
+++ trivya-frontend/src/icons/index.js
@@ -0,0 +1,38 @@
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+  fas,
+  faDice,
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+  faSliders,
+  faClock,
+  faChampagneGlasses,
+  faBan,
+  faCircle,
+  faThumbsUp,
+  faThumbsDown,
+  faKey
+} from '@fortawesome/free-solid-svg-icons'
+
+library.add(
+  fas,
+  faDice,
+  faDiceOne,
+  faDiceTwo,
+  faDiceThree,
+  faDiceFour,
+  faDiceFive,
+  faDiceSix,
+  faSliders,
+  faClock,
+  faChampagneGlasses,
+  faBan,
+  faCircle,
+  faThumbsUp,
+  faThumbsDown,
+  faKey
+)
