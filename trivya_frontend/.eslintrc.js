406fb7a6b924490ae55f2f33cb33d114261023f6
luca-bruno <lucabru@gmail.com>
2023-04-06 17:28:14 +0200

    fix: Airbnb ESLint config + tsconfig

--- trivya_frontend/.eslintrc.js
+++ trivya_frontend/.eslintrc.js
@@ -1,22 +1,25 @@
-{
+module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
     "extends": [
+        "airbnb",
+        "airbnb/hooks",
         "plugin:react/recommended",
-        "standard"
+        "plugin:@typescript-eslint/recommended"
     ],
     "overrides": [
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "plugins": [
-        "react"
+        "react",
+        "@typescript-eslint"
     ],
     "rules": {
-        "react/prop-types": "off"
     }
 }
