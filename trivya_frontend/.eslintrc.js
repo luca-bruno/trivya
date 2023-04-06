132d179b064a7236356e782b7d62a261990bc9bd
luca-bruno <lucabru@gmail.com>
2023-04-06 21:10:55 +0200

    fix: more ESLint rules + tsconfig fixes + package reshuffling

--- trivya_frontend/.eslintrc.js
+++ trivya_frontend/.eslintrc.js
@@ -1,26 +1,57 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
-    'airbnb',
-    'airbnb/hooks',
-    'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended',
+    "airbnb",
+    "airbnb/hooks",
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript"
   ],
   overrides: [
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaFeatues: {
+      jsx: true
+    },
+    ecmaVersion: "latest",
+    sourceType: "module"
   },
   plugins: [
-    'react',
-    '@typescript-eslint',
+    "react",
+    "@typescript-eslint",
+    "eslint-plugin-import",
+    "eslint-plugin-react-hooks"
   ],
-  rules: {
-    'react/jsx-filename-extension': [2, { 'extensions': ['.js', '.jsx', '.ts', '.tsx'] }],
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"]
+    },
+    "import/resolver": {
+      typescript: {},
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+        moduleDirectory: ["node_modules", "src/"]
+      }
+    }
   },
-};
+  rules: {
+    quotes: [2, "double", { avoidEscape: true }],
+    semi: [2, "never"],
+    "react/jsx-filename-extension": [2, { extensions: [".js", ".jsx", ".ts", ".tsx"] }],
+    "react/jsx-first-prop-new-line": "off",
+    "react/jsx-max-props-per-line": "off",
+    "comma-dangle": [2, "never"],
+    "react/jsx-closing-bracket-location": "off",
+    "max-len": [2, { code: 150, ignoreUrls: true }],
+    "import/no-unresolved": "error",
+    "linebreak-style": 0,
+    "import/extensions": "off"
+  }
+}
