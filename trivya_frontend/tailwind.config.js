167b1864badeaec25c7e25bcea2c3d58e83a235d
Luca Bruno <lucabru@gmail.com>
2023-08-07 00:23:20 +0200

    fix: packages pt. 2 + tailwind v3 config update

--- trivya_frontend/tailwind.config.js
+++ trivya_frontend/tailwind.config.js
@@ -1,49 +1,56 @@
-module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}"
-  ],
-  theme: {
-    screens: {
-      "2xs": "320px",
-      xs: "425px",
-      md: "768px",
-      lg: "1024px",
-      xl: "1440px",
-      "2xl": "2560px"
+import colors from "tailwindcss/colors"
+
+export const content = [
+  "./index.html",
+  "./src/**/*.{vue,js,ts,jsx,tsx}"
+]
+export const theme = {
+  screens: {
+    "2xs": "320px",
+    xs: "375px",
+    sm: "425px",
+    md: "768px",
+    lg: "1024px",
+    xl: "1440px",
+    "2xl": "2560px"
+  },
+  extend: {
+    fontFamily: {
+      primary: ["Righteous", "sans-serif"],
+      secondary: ["Rubik", "sans-serif"],
+      tertiary: ["Paytone One", "sans-serif"]
     },
-    extend: {
-      fontFamily: {
-        primary: ["Righteous", "sans-serif"],
-        secondary: ["Rubik", "sans-serif"]
-      },
-      animation: {
-        "spin-slow": "diceRoll 2s forwards"
-      },
-      keyframes: {
-        diceRoll: {
-          "0%": { transform: "rotate(0deg)" },
-          "100%": { transform: "rotate(720deg)" }
-        }
+    animation: {
+      "spin-slow": "diceRoll 2s forwards"
+    },
+    keyframes: {
+      diceRoll: {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(720deg)" }
       }
-    },
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      primary: "#F96815", // Pantone 16-1358 TCX Orange Tiger
-      secondary: "#0F4C81", // Pantone 19-4052 Classic Blue
-      tertiary: "#3f3cbb",
-      quaternary: "#3f3cbb",
-      red: "#DA291C", // Pantone 485 C
-      "red-secondary": "#831F1F", // Pantone 57 Heinz Red
-      blue: "#4589C6", // Pantone 112-22 Brady Blue
-      "blue-secondary": "#003098", // Pantone 17-4139 Azure Blue
-      green: "#01855B", // Pantone 17-6030 Jelly Bean
-      "green-secondary": "#002C17", // Pantone 627 C
-      purple: "#6050A8", // Pantone 18-3940 TN Simply Purple
-      "purple-secondary": "#501F60", // Pantone 19-3528 Imperial Purple
-      black: "#000"
     }
   },
-  plugins: []
+  colors: {
+    ...colors,
+    transparent: "transparent",
+    current: "currentColor",
+    primary: "#F96815",
+    secondary: "#0F4C81",
+    tertiary: "#3f3cbb",
+    quaternary: "#3f3cbb",
+    "red-primary": "#cc0000",
+    "red-secondary": "#b30000",
+    "red-tertiary": "#800000",
+    "blue-primary": "#4589C6",
+    "blue-secondary": "#003098",
+    "blue-tertiary": "#001f63",
+    "green-primary": "#01855B",
+    "green-secondary": "#002C17",
+    "green-tertiary": "#001c0e",
+    "purple-primary": "#6050A8",
+    "purple-secondary": "#501F60",
+    "purple-tertiary": "#32103d",
+    black: "#000"
+  }
 }
+export const plugins = []
