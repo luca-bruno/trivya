eb2a9088f2e6a7baa0ff4b3bcf5fab65a93fc3f5
Luca Bruno <lucabru@gmail.com>
2023-08-20 22:57:59 +0200

    feat: webfontloader for faster font loading

--- trivya_frontend/src/main.tsx
+++ trivya_frontend/src/main.tsx
@@ -2,22 +2,30 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 import { Provider } from "react-redux"
 import { AdminModeContextProvider } from "@contexts/AdminModeContext/AdminModeContext"
+import WebFont from "webfontloader"
 import App from "./App"
 import "./index.css"
+import "./fonts.css"
 import store from "./app/store"
 import { MalteseQuestionsContextProvider } from "./contexts/MalteseQuestionsContext/MalteseQuestionsContext"
 import { AdultModeContextProvider } from "./contexts/AdultModeContext/AdultModeContext"
 
+WebFont.load({
+  custom: {
+    families: ["Rubik", "Righteous", "Paytone One"]
+  }
+})
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLHtmlElement | DocumentFragment).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <AdminModeContextProvider>
-      <AdultModeContextProvider>
-        <MalteseQuestionsContextProvider>
-          <App />
-        </MalteseQuestionsContextProvider>
-      </AdultModeContextProvider>
-    </AdminModeContextProvider>
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <AdminModeContextProvider>
+        <AdultModeContextProvider>
+          <MalteseQuestionsContextProvider>
+            <App />
+          </MalteseQuestionsContextProvider>
+        </AdultModeContextProvider>
+      </AdminModeContextProvider>
+    </Provider>
+  </React.StrictMode>
 )
