f6beffd79b4d85ce774b4fade50bfb65dac5f00a
Luca Bruno <lucabru@gmail.com>
2023-08-20 22:39:21 +0200

    fix: AdultModeToggle styling fix

--- trivya_frontend/src/shared/AdultModeToggle/components/AdultModeToggle.tsx
+++ trivya_frontend/src/shared/AdultModeToggle/components/AdultModeToggle.tsx
@@ -17,30 +17,26 @@ const AdultModeToggle = () => {
   const onHoldEnd = () => clearTimeout(holdTimer)
 
   return (
-    <div
+    <button
+      type="button"
+      aria-label="Toggle 18+"
+      onClick={toggleAdultModeDisplay}
+      onMouseDown={onHoldStart}
+      onMouseUp={onHoldEnd}
+      onTouchStart={onHoldStart}
+      onTouchEnd={onHoldEnd}
       className={`${isDisplayingAdultMode ? "bg-secondary" : "bg-primary"} 
-    rounded-full flex justify-center items-center absolute top-3 right-3 w-12 laptopL:w-20 h-12 laptopL:h-20`}
+        "cursor-pointer select-none rounded-full flex justify-center items-center absolute top-3 right-3 w-12 laptopL:w-20 h-12 laptopL:h-20`}
     >
-      <button
-        type="button"
-        aria-label="Toggle 18+"
-        onClick={toggleAdultModeDisplay}
-        onMouseDown={onHoldStart}
-        onMouseUp={onHoldEnd}
-        onTouchStart={onHoldStart}
-        onTouchEnd={onHoldEnd}
-        className="cursor-pointer select-none"
-      >
-        <p className={`${!isDisplayingAdultMode ? "text-secondary" : "text-primary"} text-3xl laptopL:text-5xl font-bold z-10`}>18</p>
+      <p className={`${!isDisplayingAdultMode ? "text-secondary" : "text-primary"} text-3xl laptopL:text-5xl font-bold z-10`}>18</p>
 
-        {isDisplayingAdultMode && (
-          <FontAwesomeIcon
-            icon={["fas", "ban"]}
-            className="text-red-primary text-5xl laptopL:text-7xl absolute top-0 right-0 laptopL:right-0 w-12 laptopL:w-20 h-12 laptopL:h-20 z-20"
-          />
-        )}
-      </button>
-    </div>
+      {isDisplayingAdultMode && (
+        <FontAwesomeIcon
+          icon={["fas", "ban"]}
+          className="text-red-primary text-5xl laptopL:text-7xl absolute top-0 right-0 laptopL:right-0 w-12 laptopL:w-20 h-12 laptopL:h-20 z-20"
+        />
+      )}
+    </button>
   )
 }
 
