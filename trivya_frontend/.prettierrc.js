cb3ac49f89e3830ec4a69920b86ba42e1f4bf8a9
Luca Bruno <lucabru@gmail.com>
2025-01-14 00:30:23 +0100

    chore: prettierrc

---
+++ trivya_frontend/.prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    arrowParens: "avoid",
+    bracketSpacing: true,
+    printWidth: 150,
+    semi: false,
+    singleAttributePerLine: false,
+    singleQuote: false,
+    trailingComma: "none",
+    tabWidth: 2,
+}
